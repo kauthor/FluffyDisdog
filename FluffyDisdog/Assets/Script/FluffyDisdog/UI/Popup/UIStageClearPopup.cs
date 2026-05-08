7b1707d0816318478705fa92fc6e9ba90a4bdbfe
kauthor <kim15883082@naver.com>
2026-05-08 21:52:51 +0900

    동전이 콸콸콸

--- FluffyDisdog/Assets/Script/FluffyDisdog/UI/Popup/UIStageClearPopup.cs
+++ FluffyDisdog/Assets/Script/FluffyDisdog/UI/Popup/UIStageClearPopup.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using DG.Tweening;
 using FluffyDisdog.Manager;
 using UnityEngine;
 
@@ -10,6 +11,10 @@ namespace FluffyDisdog.UI
 
         [SerializeField] private GoldCasher casher;
         [SerializeField] private OutlinedText txtGainedGold;
+
+        [SerializeField] private GameObject[] CoinAnimPrefabs;
+        [SerializeField] private Transform coinStartTr;
+        [SerializeField] private Transform coinEndTr;
 
         private int gainGold;
         private int goalGold;
@@ -46,6 +51,8 @@ namespace FluffyDisdog.UI
             var currentUIGainGold =gainGold;
 
             float temp = 0;
+            float coinTerm = 0.1f;
+            float coinTemp = 0;
             while (temp < time)
             {
                 casher.SyncGold(startGold + (int)((float)gainGold* temp/time));
@@ -53,6 +60,14 @@ namespace FluffyDisdog.UI
                 txtGainedGold.SetText(currentUIGainGold.ToString()+" G");
                 yield return new WaitForFixedUpdate();
                 temp += Time.fixedDeltaTime;
+                coinTemp += Time.fixedDeltaTime;
+                if (coinTemp > coinTerm)
+                {
+                    coinTemp = 0;
+                    var newCoin = GameObject.Instantiate(CoinAnimPrefabs[Random.Range(0,3)], coinStartTr);
+                    newCoin.transform.DOMoveX(coinEndTr.position.x, 0.5f).SetEase(Random.Range(0,2)==0? Ease.OutQuad:Ease.InQuad);
+                    newCoin.transform.DOMoveY(coinEndTr.position.y, 0.5f).SetEase(Random.Range(0,2)==0? Ease.OutQuad:Ease.InQuad).onComplete += ()=>newCoin.gameObject.SetActive(false);
+                }
             }
             
             casher.SyncGold(goalGold);
